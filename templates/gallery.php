45acd51038c81d40079b01db323deaa581c3f371
Kaspars <kasparsj@gmail.com>
2013-01-26 09:04:40 +0000

    option to skip css by style filter

--- templates/gallery.php
+++ templates/gallery.php
@@ -1,6 +1,8 @@
 <style type="text/css">
     .gallery-slider { overflow: hidden; width: <?=$width?>px; height: <?=$height?>px; }
+<?php if (apply_filters('use_default_gallery_style', true)): ?>
     <?=$options['gallery_css']?>
+<?php endif; ?>
 </style>
 <div id="gallery-<?=$instance?>" class="gallery galleryid-<?=$id?>">
 <?php
